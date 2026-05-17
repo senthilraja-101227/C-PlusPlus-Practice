bedab801cc233a2243b177eb224f32a584fcec80
senthilraja-101227 <2703subasri@gmail.com>
2026-05-17 23:03:05 +0530

    Day 9

--- day9.cpp
+++ day9.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
  //sorting
-/* int n;
+ int n;
  cin>>n;
  int arr[n];
  for(int i=0;i<n;i++){
@@ -24,25 +24,25 @@ int main(){
 
  // I am not going depth into sorting alogorithms just found one difference between sorting and selection sorting
  //selection sorting find the min element in a array and after that it swap it but in normal sorting we compare one by one if satisfies we swap there may be multiple swap
-  int n1;
-  cin>>n1;
-  int arr[n1];
-  for(int i=0;i<n1;i++){
+  int num;
+  cin>>num;
+  int arr[num];
+  for(int i=0;i<num;i++){
     cin>>arr[i];
   }
-  for(int i=0;i<n1-1;i++){
+  for(int i=0;i<num-1;i++){
         int minIndex=i;
-    for(int j=i+1;j<n1;j++){
+    for(int j=i+1;j<num;j++){
         if(arr[j]<arr[minIndex]){
             minIndex=j;
         }
     }
   swap(arr[i],arr[minIndex]);
   }
-  for(int i=0;i<n1;i++){
+  for(int i=0;i<num;i++){
     cout<<arr[i]<<" ";
   }
-  */
+
   //matrix multiplication
 
   int n1,m1;
