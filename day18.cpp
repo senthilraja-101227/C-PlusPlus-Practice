83d7257eb223fb0276399bcd84adc36161b6cb23
senthilraja-101227 <2703subasri@gmail.com>
2026-05-28 23:12:19 +0530

    Day 18

--- day18.cpp
+++ day18.cpp
@@ -1,1 +1,14 @@
+//Revision ->>day 6
 
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+ string s;
+ getline(cin,s);
+// reverse(s.begin(),s.end());
+ //cout<<s;
+ for(int i=s.length()-1;i>=0;i--){
+    cout<<s[i];
+ }
+}
